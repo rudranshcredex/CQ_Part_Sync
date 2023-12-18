00dfe8ad00671379026f32020ba605ffa46e6be6
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2023-12-18 10:54:33 +0530

    unit tests

--- force-app/main/default/lwc/utilityButton/utilityButton.js
+++ force-app/main/default/lwc/utilityButton/utilityButton.js
@@ -7,26 +7,9 @@ export default class UtilityButton extends LightningElement {
     handleButton() {
         getRecordsMethod().then(results => {
             console.log('success----->', results);
-            
-                const event = new ShowToastEvent({
-                    title: 'Success',
-                    message: 'Records Inserted',
-                    variant: 'success',
-                    mode: 'dismissable'
-                });
-                this.dispatchEvent(event);
-            
-   
         })
         .catch(error => {
                 console.error('Error---->', error);
-                const event = new ShowToastEvent({
-                    title: 'Success',
-                    message: 'Insert Failed',
-                    variant: 'error',
-                    mode: 'dismissable'
-                });
-                this.dispatchEvent(event);
             });
     }
 }
